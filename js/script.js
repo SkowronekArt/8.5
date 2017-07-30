08f7078447eb489e5605e4d7200ce39a657ccbc7
ANNA Skowronek-Jaskuła <annaskowronek-jaskula@MacBook-Air-ANNA.local>
2017-07-30 19:18:40 +0200

    revision 1 - 2017-07-31

--- js/script.js
+++ js/script.js
@@ -17,9 +17,9 @@ Wyślij zmiany na nowe repozytorium na GitHubie. Pamiętaj o udostępnieniu go m
 
 */
 
-var a 				= prompt("Podaj szerokość podstawy trójkąta", 5);
-var h 				= prompt("Podaj wysokość trójkąta", 5);
-	triangleArea 	= a * h / 2;
+var a = prompt("Podaj szerokość podstawy trójkąta", 5);
+    h = prompt("Podaj wysokość trójkąta", 5);
+    triangleArea 	= a * h / 2;
 
 
 
@@ -27,3 +27,6 @@ document.getElementById("pole").innerHTML =
 	"Pole trójkąta o podstawie a równej " + a + " i wysokości h równej " + h + " jest równe: " + triangleArea;
 
 console.log('Pole trójkąta o podstawie a równej ' + a + ' i wysokości h równej ' + h + ' jest równe: ' + triangleArea);
+
+
+// nie ma tabów :)
